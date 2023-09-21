6fb4940f37db39a2a53718083e7ae7d71c734917
Nicholas Heeralal <nicholas.heeralal@mail.utoronto.ca>
2023-09-20 20:32:50 -0400

    Activity 4: Final submission for this lab that includes the relevant screenshots of the second half of activity 4. Note in these screenshots, I used port 50000 instead of 5000 as it was being used for another program

--- Activity_4/hello.py
+++ Activity_4/hello.py
@@ -4,12 +4,13 @@ from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, ValidationError
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT email address?', validators=[DataRequired(), Email(message="Please include @ in the email address", granular_message=True)])
     submit = SubmitField('Submit')
-
+    
 app = Flask(__name__)
 
 moment = Moment(app)
@@ -21,12 +22,21 @@ bootstrap = Bootstrap(app)
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        if form.validate_on_submit():
+
+            old_name = session.get('name')
+            old_email = session.get('email')
+            if old_name is not None and old_name != form.name.data:
+                flash('Looks like you have changed your name!') 
+
+            if old_email is not None and old_email !=form.email.data:
+                flash('Looks like you have changed your email!')
+
             session['name'] = form.name.data
-            return redirect(url_for('index'))
-    return render_template('index.html', form = form, name = session.get('name'))
+            session['email'] = form.email.data
+        return redirect(url_for('index'))
+        
+    return render_template('index.html', form = form, name = session.get('name'), email = session.get('email'))
 
 @app.route('/user/<name>')
 def user(name):
--- README.md
+++ README.md
@@ -15,3 +15,16 @@ Screenshot showing my web page with
 2. “Hello [your name]!” as the title
 3. a timestamp in ‘LLLL’ format.
 ![Screenshot of Activity 3_Lab_1](/Screenshots/Activity_3_Lab_1.png)
+
+### Activity 4
+Activity 4 Step 2. Screenshot showing addition of one more field for filling in email address. 
+![Screenshot of Activity 4_Step_2](Screenshots/Activity_4_2_Lab_1.png)
+
+Activity 4 Step 3. Screenshot showing filling in my first name and my UofT email address, then clicking submit.
+![Screenshot of Activity 4_Step_3](Screenshots/Activity_4_3_Lab_1.png)
+
+Activity 4 Step 4. Screenshot showing filling in my first name and last name in the 1st field, and filling in my first name in the 2nd field, then clicking submit and received an error message  
+![Screenshot of Activity 4_Step_4](Screenshots/Activity_4_4_Lab_1.png)
+
+Activity 4 Step 5. Screenshot showing filling in my first name and last name in the 1st field, and filling in my NON UofT email in the 2nd field, then clicking submit.  
+![Screenshot of Activity 4__Step_5](Screenshots/Activity_4_5_Lab_1.png)
