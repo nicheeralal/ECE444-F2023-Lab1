023fbcd511c4f4fdf307ce337e172ed34273dd0a
Nicholas Heeralal <nicholas.heeralal@mail.utoronto.ca>
2023-10-02 23:04:48 -0400

    Lab 3 Activity 3: Added screenshot and the change to hello.py to include Docker message. I also renamed the other folders to avoid any potential confusion

---
+++ Lab3_Activity_3/hello.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, session, redirect, url_for, flash
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Email, ValidationError
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT email address?', validators=[DataRequired(), Email(message="Please include @ in the email address", granular_message=True)])
+    submit = SubmitField('Submit')
+    
+app = Flask(__name__)
+
+moment = Moment(app)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+bootstrap = Bootstrap(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        if form.validate_on_submit():
+
+            old_name = session.get('name')
+            old_email = session.get('email')
+            if old_name is not None and old_name != form.name.data:
+                flash('Looks like you have changed your name!') 
+
+            if old_email is not None and old_email !=form.email.data:
+                flash('Looks like you have changed your email!')
+
+            session['name'] = form.name.data
+            session['email'] = form.email.data
+        return redirect(url_for('index'))
+        
+    return render_template('index.html', form = form, name = session.get('name'), email = session.get('email'))
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name, current_time=datetime.utcnow())
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
--- README.md
+++ README.md
@@ -11,4 +11,11 @@ If you are reading this, it is likely because you are reading from the "lab3" br
 Screenshot showing a screenshot showing that I have successfully finished the task into your README file
 on the lab3 branch. E.g. print the version of Docker.
 
-![Screenshot of Activity 2](/Screenshots/Lab3/Activity1_Lab3.png)
+![Screenshot of Lab 3 Activity 2](/Screenshots/Lab3/Activity2_Lab3.png)
+
+### Activity 3
+
+ Here is a screenshot of the application running locally with Change “Hello [your name]!” to “Hello [your name]! Welcome to Lab3 Docker!” as the title
+of your webpage
+
+![Screenshot of Lab 3 Activity 3](/Screenshots/Lab3/Activity3_Lab3.png)
